77785c899507dafafb4aa1671c33196965891750
klion-gh <call.me.klion@gmail.com>
2026-09-04 09:33:26 +0300

    release: 1.14.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.phantom.vpn"
         minSdk = 24
         targetSdk = 34
-        versionCode = 15
-        versionName = "1.13.0"
+        versionCode = 16
+        versionName = "1.14.0"
     }
 
     signingConfigs {
